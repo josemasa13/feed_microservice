ed50e24b8f43202cb64196f3a4c47460bd9966f4
Jose <quirroone@gmail.com>
2020-05-14 10:38:13 -0500

    Add Docker file

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=
-export POSTGRES_PASSWORD=
-export POSTGRES_HOST=
-export POSTGRES_DB=
-export AWS_BUCKET=
-export AWS_REGION=
+export POSTGRES_USERNAME=administrator
+export POSTGRES_PASSWORD=admin123
+export POSTGRES_HOST=cdr.czhjnmvrd1fr.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=udagram-972405523754-dev
+export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=hello
 export URL=http://localhost:8100
--- src/controllers/v0/index.router.ts
+++ src/controllers/v0/index.router.ts
@@ -4,7 +4,6 @@ import {FeedRouter} from './feed/routes/feed.router';
 const router: Router = Router();
 
 router.use('/feed', FeedRouter);
-
 router.get('/', async (req: Request, res: Response) => {
   res.send(`V0`);
 });
